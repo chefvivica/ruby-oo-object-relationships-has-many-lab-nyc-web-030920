eda096e982d7ca1d11efcdc7e64b0676143c3e52
Vivica Ma <vivicacongma@gmail.com>
2020-03-11 14:35:48 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -10,24 +10,9 @@ class Song
     @@all
   end 
   
+  def artist_name 
+    artist_name if artist
+  end 
   
 end 
 
-# class Song
-#   attr_accessor :name, :artist
-
-#   @@all = []
-
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-
-#   def artist_name
-#     artist.name if artist
-#   end
-# end
