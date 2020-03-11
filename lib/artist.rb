a60f49486826fa9bb4f69b4ab16f34f28c3b14ab
Vivica Ma <vivicacongma@gmail.com>
2020-03-11 14:54:39 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,39 +1,3 @@
-# class Artist
-#   attr_accessor :name
-#   @@all = []
-#   def initialize(name)
-#     @name = name
-#     @@all << self
-#   end
-
-#   def self.all
-#     @@all
-#   end
-    
-#   def songs 
-#     Song.all.select {|song| song.artist == slef}
-
-#   end 
-  
-#   def add_song_by_name(name)
-#     song = Song.new(name)
-#     song.artist = self
-#   end
-
-#   def add_song(song)
-#     song.artist = self
-#   end
-
-#   def add_song_by_name(name)
-#     song = Song.new(name)
-#     song.artist = self
-#   end
-
-#   def self.song_count
-#     Song.all.count
-#   end
-# end
-
 class Artist
   attr_accessor :name
 
@@ -43,26 +7,55 @@ class Artist
     @name = name
     @@all << self
   end
-  
-end 
-#   def self.all
-#     @@all
-#   end
 
-#   def add_song(song)
-#     song.artist = self
-#   end
+  def self.all
+    @@all
+  end
 
-#   def add_song_by_name(name)
-#     song = Song.new(name)
-#     song.artist = self
-#   end
+  def add_song(song)
+    song.artist = self
+  end
 
-#   def songs
-#     Song.all.select {|song| song.artist == self}
-#   end
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
 
-#   def self.song_count
-#     Song.all.count
-#   end
-# end
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+endclass Artist
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_song(song)
+    song.artist = self
+  end
+
+  def add_song_by_name(name)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def songs
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.song_count
+    Song.all.count
+  end
+end
