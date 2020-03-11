c993242bd06993f355fa851cdf2c56399a2c6cb4
Vivica Ma <vivicacongma@gmail.com>
2020-03-11 14:53:22 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -43,25 +43,25 @@ class Artist
     @name = name
     @@all << self
   end
+end 
+#   def self.all
+#     @@all
+#   end
 
-  def self.all
-    @@all
-  end
+#   def add_song(song)
+#     song.artist = self
+#   end
 
-  def add_song(song)
-    song.artist = self
-  end
+#   def add_song_by_name(name)
+#     song = Song.new(name)
+#     song.artist = self
+#   end
 
-  def add_song_by_name(name)
-    song = Song.new(name)
-    song.artist = self
-  end
+#   def songs
+#     Song.all.select {|song| song.artist == self}
+#   end
 
-  def songs
-    Song.all.select {|song| song.artist == self}
-  end
-
-  def self.song_count
-    Song.all.count
-  end
-end
+#   def self.song_count
+#     Song.all.count
+#   end
+# end
